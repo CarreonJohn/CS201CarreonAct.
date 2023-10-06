77a83c3cbcf63b2f4c8af8561fc09dbb5bb73ab1
CarreonJohn <146902586+CarreonJohn@users.noreply.github.com>
2023-10-06 11:19:07 +0800

    Add files via upload
    BSCS2A
    CARREON, John Andrei

---
+++ DSA Act 1/act1.js
@@ -0,0 +1,11 @@
+let FirstFavNum = 666;
+let SecondFavNum = 7;
+console.log(FirstFavNum + SecondFavNum);
+console.log(FirstFavNum - SecondFavNum);
+console.log(FirstFavNum / SecondFavNum);
+console.log(FirstFavNum * SecondFavNum);
+console.log(FirstFavNum % SecondFavNum);
+
+let Power = Math.pow(FirstFavNum);
+console.log(Power);
+
